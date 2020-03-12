c688fe4599ec5d91ab600633524ec6e1df50151b
chris296 <chris.rios296@gmail.com>
2020-03-12 13:34:20 -0700

    finished

--- src/hooks/useDarkMode.jsx
+++ src/hooks/useDarkMode.jsx
@@ -2,5 +2,15 @@ import {useLocalStorage} from './useLocalStorage';
 import {useEffect} from 'react';
 
 export const useDarkMode = () => {
-    const [someValue, setSomeValue] = useLocalStorage();
+    const [value, setValue] = useLocalStorage('darkmode', false);
+
+    useEffect(() => {
+        if(value === true){
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    },[value]);
+
+    return [value, setValue];
 }
